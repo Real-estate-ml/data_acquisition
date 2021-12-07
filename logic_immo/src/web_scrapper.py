5f390061e199036aa81b341fcda9eae25f8c3984
Yinshangyi <jl.dejaeger@gmail.com>
2021-12-07 21:33:51 +0100

    Get all the links and downloading the HTML file to the file system

--- logic_immo/src/web_scrapper.py
+++ logic_immo/src/web_scrapper.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 from time import sleep
+from pathlib import Path
 
 class WebScrapper():
     def __init__(self, url):
@@ -28,35 +29,75 @@ class WebScrapper():
         html_soup = BeautifulSoup(page.text, "html.parser")
         return html_soup
 
-    def get_page_number(self, home_page: BeautifulSoup)->int:
+    def get_apartment_ad(self, link: str):
+        """[summary]
+
+        :param link: [description]
+        :type link: str
+        :return: [description]
+        :rtype: [type]
+        """
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36'}
+        page = requests.get(link, headers=headers)
+        print(page)
+        #return page.text
+
+    def set_last_page_number(self, home_page):
         """The aim of this function is to return the number of page from logic_immo website in order to get all the real-estate ads
         :param home_page: home page
         :type home_page: BeautifulSoup object
-        :return: total number of pages
-        :rtype: int
         """
         number = home_page.find("h1", {"class": "titleSearch"}).text
         numbers = [int(nbr) for nbr in number.split() if nbr.isdigit()]
         pages_number = int(numbers[0]/20)
-        return pages_number
+        self.nb_pages = pages_number
 
-    def get_all_pages_links(self):
+    def set_all_pages_links(self):
         """Get all the apartment ads links
         :return: total links of all pages
         :rtype: list
         """
-        for i in range(0, self.get_page_number(self.url)):
-            soup = BeautifulSoup(self.get_html_page(i).text, "html.parser")
-            links = soup.find_all('a', {"class": "linkToFa"})
-            for j in links:
-                self.links.append(j['href'])
-            sleep(1.1)
-        return self.links
+        for i in range(1, self.nb_pages+1):
+            html_page = self.get_html_page(i)
+            page_links = self.get_all_links_for_page(html_page)
+            for link in page_links:
+                self.links.append(link)
+            sleep(3)
+
+    def get_all_links_for_page(self, html_page):
+        """Get all the links for a given page
+
+        :param html_page: HTML Page
+        :type html_page: BeautifulSoup
+        :return: List of all the links
+        :rtype: list[str]
+        """
+        links = html_page.find_all('a', {"class": "linkToFa"})
+        return [link['href'] for link in links]
+
 
     def export_to_gcs(self):
         """Export the HTML to Google Cloud Storage
         """
-        None
+        Path("export/html").mkdir(parents=True, exist_ok=True)
+        for link in self.links:
+            ad_page = self.get_apartment_ad(link)
+            sleep(3)
+            
 
+# Test
+BASE_URL = "https://www.logic-immo.com/vente-immobilier-ile-de-france,1_0/options/groupprptypesids=1"
+web_scrapper = WebScrapper(BASE_URL)
 
+# Set total number of pages
+home_page = web_scrapper.get_html_page(None)
+web_scrapper.set_last_page_number(home_page)
+print("There are {} pages".format(web_scrapper.nb_pages))
+
+# Set all links
+web_scrapper.nb_pages = 2
+web_scrapper.set_all_pages_links()
+
+# Export all links
+web_scrapper.export_to_gcs()
 
--- logic_immo/test/test_web_scrapper.py
+++ logic_immo/test/test_web_scrapper.py
@@ -6,6 +6,15 @@ def test_get_page_number():
     f = open("test/html_test/home_page.html", "r")
     home_page = BeautifulSoup(f.read(), "html.parser")
     f.close()
-    expected_pages_number = 3328
-    actual_pages_number = web_scrapper.get_page_number(home_page)
-    assert actual_pages_number == expected_pages_number
+    expected_pages_number = 3326
+    web_scrapper.set_last_page_number(home_page)
+    assert web_scrapper.nb_pages == expected_pages_number
+
+def test_get_all_links_for_page():
+    web_scrapper = WebScrapper("base_url")
+    f = open("test/html_test/home_page.html", "r")
+    home_page = BeautifulSoup(f.read(), "html.parser")
+    f.close()
+    expected_found_links = 21
+    found_links = web_scrapper.get_all_links_for_page(home_page)
+    assert len(found_links) == expected_found_links
